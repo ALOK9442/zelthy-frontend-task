d5dbaeed598fbbba91f3b3a401e08c1c2cd6320f
pewpewXD <104527572+ALOK9442@users.noreply.github.com>
2025-02-15 16:53:17 +0530

    Update logo

--- src/utils/logo.jsx
+++ src/utils/logo.jsx
@@ -2,7 +2,7 @@ import TextAvatar from "./avatar";
 
 export default function Logo() {
   return (
-    <h1 className="sm:text-2xl font-bold text-blue-600 flex gap-1 items-center">
+    <h1 className="sm:text-2xl font-bold text-blue-600 flex gap-1 items-center" onClick={() => navigate("/")}>
       <TextAvatar name={"S"} /> Schedulerly
     </h1>
   );
