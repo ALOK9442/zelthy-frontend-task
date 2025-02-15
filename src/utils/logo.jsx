962f77b4d6ac85bae30102fd954e497871e430d7
pewpewXD <104527572+ALOK9442@users.noreply.github.com>
2025-02-15 16:55:59 +0530

    Update logo and routing

--- src/utils/logo.jsx
+++ src/utils/logo.jsx
@@ -1,6 +1,8 @@
 import TextAvatar from "./avatar";
+import { useNavigate } from "react-router-dom";
 
 export default function Logo() {
+  const navigate = useNavigate();
   return (
     <h1 className="sm:text-2xl font-bold text-blue-600 flex gap-1 items-center" onClick={() => navigate("/")}>
       <TextAvatar name={"S"} /> Schedulerly
