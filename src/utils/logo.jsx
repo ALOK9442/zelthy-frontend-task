a4d70437e40b25e2cc793b9056ac1ada83e29ea0
ALOK9442 <alok2110036@akgec.ac.in>
2025-02-15 17:00:57 +0530

    dependdency and ui changes

--- src/utils/logo.jsx
+++ src/utils/logo.jsx
@@ -1,8 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import TextAvatar from "./avatar";
 
 export default function Logo() {
+  const navigate = useNavigate();
   return (
-    <h1 className="sm:text-2xl font-bold text-blue-600 flex gap-1 items-center">
+    <h1 className="sm:text-2xl font-bold text-blue-600 flex gap-1 items-center" onClick={() => navigate("/")}>
       <TextAvatar name={"S"} /> Schedulerly
     </h1>
   );
